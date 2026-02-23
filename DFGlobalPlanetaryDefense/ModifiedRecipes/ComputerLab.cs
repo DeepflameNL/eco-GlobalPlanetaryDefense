9895a03127ea8e3b37cc64765254ef01f8960367
Kai <coilysiren@gmail.com>
2026-02-22 22:22:22 -0800

    this dot

--- DFGlobalPlanetaryDefense/ModifiedRecipes/ComputerLab.cs
+++ DFGlobalPlanetaryDefense/ModifiedRecipes/ComputerLab.cs
@@ -7,12 +7,12 @@ namespace Eco.Mods.TechTree
     {
         partial void ModsPreInitialize()
         {
-            Recipes[0].Ingredients.Clear();
-            Recipes[0].Ingredients.Add(new IngredientElement(typeof(CentralProcessingCoreItem), 1, true));
-            Recipes[0].Ingredients.Add(new IngredientElement(typeof(MagneticTapeArrayItem), 1, true));
-            Recipes[0].Ingredients.Add(new IngredientElement(typeof(RadarTransceiverItem), 1, true));
-            Recipes[0].Ingredients.Add(new IngredientElement(typeof(HighPerformancePowerRackItem), 1, true));
-            Recipes[0].Ingredients.Add(new IngredientElement(typeof(CoolantCirculationSystemItem), 1, true));
+            this.Recipes[0].Ingredients.Clear();
+            this.Recipes[0].Ingredients.Add(new IngredientElement(typeof(CentralProcessingCoreItem), 1, true));
+            this.Recipes[0].Ingredients.Add(new IngredientElement(typeof(MagneticTapeArrayItem), 1, true));
+            this.Recipes[0].Ingredients.Add(new IngredientElement(typeof(RadarTransceiverItem), 1, true));
+            this.Recipes[0].Ingredients.Add(new IngredientElement(typeof(HighPerformancePowerRackItem), 1, true));
+            this.Recipes[0].Ingredients.Add(new IngredientElement(typeof(CoolantCirculationSystemItem), 1, true));
         }
     }
 }
